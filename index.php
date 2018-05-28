73707a9adb52b39a1f4aae6d724fb7c1960af503
Oluwagbuyi Abimbola <bimbowande@gmail.com>
2018-05-28 15:09:27 +0100

    file_uploaded

---
+++ index.php
@@ -0,0 +1,160 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="styles/meter.css">
+</head>
+<body>
+	<section class="container-style">
+		<div class="design-section">
+			<div class="meter-design">
+				<div class="meter-layer">
+					<div class="meter-layer-2">
+						<div class="meter-layer-3">
+							<div class="meter-layer-content">
+								<div class="indicator-section">
+									<div class="indicator-content">
+										<div class="indicator">
+											<div class=" indicator-light indicator-light-1"></div>
+											 <h3>Low Credit</h3>
+										</div>
+										<div class="indicator">
+											<div class=" indicator-light indicator-light-2"></div>
+											<h3>Cover Open</h3>
+										</div>
+										<div class="indicator">
+											<div class=" indicator-light indicator-light-3"></div>
+											<h3>Disconnect</h3>
+										</div>
+										<div class="indicator">
+											<div class=" indicator-light indicator-light-4"></div>
+											<h3>Overload</h3>
+										</div>
+										<div class="indicator">
+											<div class=" indicator-light indicator-light-4"></div>
+											<h3>Comm</h3>
+										</div>
+										<div class="sensor">
+											
+										</div>
+								  </div>
+								</div>
+								
+								<!-- Meter Screen content -->
+								<div class="meter-screen">
+									<div class="meter-screen-content">
+										<div class="meter-content-display"></div>
+									</div>
+								</div>
+								<!-- End of  Meter Screen content -->
+								<div class="meter-text">
+									<div class="text-1">
+										<h2>FUTAEEE</h2>
+										<span>&copy;</span>
+										<h3>H1000</h3>
+									</div>
+									<div class="text-2">
+										<h4>Customer Interface Unit</h4>
+									</div>
+									
+									<!-- Text 3-->
+									<div class="text-3">
+										<div class="isbn-image">
+											<img src="./images/ISBN.jpg" alt="">
+										</div>
+										<div class="status-code">
+											<div class="status-text">
+												<div class="text-content">
+													<h4>Emergency Code</h4>
+													<p>44444</p>
+												</div>
+												<div class="text-content">
+													<h4>Manual Connect</h4>
+													<p>88888</p>
+												</div>
+												<div class="text-content">
+													<h4>Battery WakeUp</h4>
+													<p>3</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- end of Text 3-->
+								</div>
+								<div class="btn-content">
+									<div class="btn">
+										<button class="btn-box">1</button>
+										<button class="btn-box">2</button>
+										<button class="btn-box">3</button>
+										<button class="btn-box">4</button>
+										<button class="btn-box">5</button>
+										<button class="btn-box">6</button>
+										<button class="btn-box">7</button>
+										<button class="btn-box">8</button>
+										<button class="btn-box">9</button>
+										<button class="btn-box">10</button>
+										<button class="btn-box">11</button>
+										<button class="btn-box">12</button>
+									</div>
+									<div class="btn-command">
+										<div class="command-text">
+											<h4>01</h4>
+											<p>Current Available Credit</p>
+										</div>
+										<div class="command-text">
+											<h4>02</h4>
+											<p>Current Consumption Rate</p>
+										</div>
+										<div class="command-text">
+											<h4>03</h4>
+											<p>Average Daily Consumption</p>
+										</div>
+										<div class="command-text">
+											<h4>04</h4>
+											<p>Estimated Days Left</p>
+										</div>
+										<div class="command-text">
+											<h4>05</h4>
+											<p>Last Billing Date</p>
+										</div>
+										<div class="command-text">
+											<h4>06</h4>
+											<p>Last Billed Amount</p>
+										</div>
+										<div class="command-text">
+											<h4>07</h4>
+											<p>Total Credit Recharge</p>
+										</div>
+										<div class="command-text">
+											<h4>08</h4>
+											<p>Total Credit Used</p>
+										</div>
+										<div class="command-text">
+											<h4>09</h4>
+											<p>Current Power</p>
+										</div>
+										<div class="command-text">
+											<h4>10</h4>
+											<p>Meter Number</p>
+										</div>
+										<div class="command-text">
+											<h4>11</h4>
+											<p>Supply Group Code</p>
+										</div>
+										<div class="command-text">
+											<h4>12</h4>
+											<p>Tarrif Index</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div >
+	</section>
+</body>
+</html>
