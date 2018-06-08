3ada80ae9fd4091843c5975704716aff712a583b
Oluwagbuyi Abimbola <bimbowande@gmail.com>
2018-06-08 17:49:05 +0100

    life-updates

--- js/display.js
+++ js/display.js
@@ -6,20 +6,50 @@ $(()=>{
         meter_details.forEach((ele)=>{
             newData = ele;
         })
-       function consumeEnergy(availablePower){
-            $('.power-output').text(availablePower + ' kw');
-           
-       }
+        //meter_id
+        let meter_id = parseInt(newData.id);
+        let availablePower = parseFloat(newData.available_credit);
 
-       let availablePower = parseFloat(newData.available_credit);
+        /**
+         * method for changing the available power
+         */
+            function consumeEnergy(availablePower){
+                $('.power-output').text(availablePower + ' kw');   
+            }
 
-       setInterval(()=>{
-           availablePower= availablePower - 0.19999978 ;
-          consumeEnergy(availablePower);
-       },2200)
+       /**
+        *  method for sending updated power
+        */
+            let updates = (availablePower,meter_id) => {
+                let data_parsed = {
+                    availablePower,
+                    meter_id
+                }
+                /**
+                 *  Post data to the database
+                 */
+                $.ajax({
+                    url:"./scripts/update.php",
+                    type:"POST",
+                    data:data_parsed,
+                    success:function(status,data){
+                        console.log(data_parsed);
+                    },
+                    failure:function(){
+                        console.log('failure')
+                    }
+                })
+            }
+
+            //updating the database on interval
+            let autoUpdate = setInterval(()=>{
+                availablePower= availablePower - 0.19999978 ;
+                consumeEnergy(availablePower);
+                updates(availablePower,meter_id);
+            },2200)
 
        
         $('.text-1 h2').text(newData.meter_number);
 
     })
-})
+})                           
---
+++ scripts/update.php
@@ -0,0 +1,24 @@
+<?php
+include('./ConnectInfo.php');
+class UpdateData{
+    private $query;
+    public function __construct(){
+        $testConn = new ConnectInfo();
+        $this->connDB =  $testConn->connect();
+    } 
+    public function getUpdates($availablePower,$meter_id){
+        if($availablePower > 0){
+            $this->query = "UPDATE meter_details SET available_credit = '$availablePower' WHERE id='$meter_id'";
+            $this->result = mysqli_query($this->connDB,$this->query);
+            echo  $error = $this->result ? 'succesful upload':$this->result->error;
+        }
+    }
+}
+
+$updates =  new UpdateData();
+if(isset($_POST)){
+    $availablePower = $_POST['availablePower'];
+    $meter_id = $_POST['meter_id'];
+    $updates->getUpdates($availablePower,$meter_id);
+} else echo'no data sent';
+?>
