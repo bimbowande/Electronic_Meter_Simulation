4ea48e0f50e2cf571136bb59f7e7c2162073ad79
Oluwagbuyi Abimbola <bimbowande@gmail.com>
2018-06-21 12:27:02 +0100

    action_btn

--- js/getUpdates.js
+++ js/getUpdates.js
@@ -1,12 +1,23 @@
 $(()=>{
+    _parsedData = {};
     let _recievedData  = (_dataRecieved) => {
-        console.log(_dataRecieved);
+      
+         _dataRecieved = JSON.parse(_dataRecieved).forEach(element => {
+             _parsedData = element;  
+        });
+        return _parsedData;
     }
 
-
+   let RefreshELem = data_func => {
+       let {id,meter_number,availablePower,available_credit} = data_func;
+       console.log(available_credit);
+      
+   }
     let _getUpdates = setInterval(()=>{
         $.get('./scripts/getUpdates.php',(data,status)=>{
-            _recievedData(data);
+           RefreshELem(_recievedData(data));
         })
     },2200)
+
+
 })
