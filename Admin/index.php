a1bc8229f497970ee7efe4bc44bed7fc4db8d913
Abimbola Oluwagbuyi <oluwagbuyi01@yahoo.com>
2019-01-22 17:45:49 +0100

    Admin folder

---
+++ Admin/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+			<!-- Bootstrap -->
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+			<!-- admin styles --->
+	<link rel="stylesheet" href="../styles/admin-login.css">
+</head>
+<body>
+		<!--  Jquery--->
+	<script src='../js/jquery.min.js'></script>
+		<!--  Bootstrap --->
+	<script src='../js/bootstrap.js'></script>
+</body>
+</html>
